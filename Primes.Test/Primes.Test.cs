8f9447ba18c64d3cb493db48f9e7ab0a57b72955
Steve Larsen, VRC Metal Systems <Steve.Larsen@vrcmetalsystems.com>
2019-03-18 23:38:09 -0600

    Checking in Changes - I have 2 tests already

--- Primes.Test/Primes.Test.cs
+++ Primes.Test/Primes.Test.cs
@@ -8,24 +8,39 @@ namespace Primes.Test
     [TestClass]
     public class PrimesTester
     {
+        List<int> empty = new List<int>();
+
         [TestInitialize]
         public void PrimesInitialize()
         {
             // Not needed as Primes is a Static Class
+            
+        }
+
+        [TestMethod]
+        public void GivenAValueOfZero_TheExpectedResultIsAnEmptyList()
+        {
+            // Arrange - Setup the values for the function
+            var Zero = 0;
+
+            // Act - Perform an operation
+            var result = Primes.FindPrimes(Zero);
+
+            // Assert - Check for validity
+            Assert.IsTrue(empty.Count == result.Count && empty.Count == 0);
         }
 
         [TestMethod]
         public void GivenAValueOfOne_TheExpectedResultIsAnEmptyList()
         {
             // Arrange - Setup the values for the function
-            var One = 1;
-            var empty = new List<int>();
+            var One = 1;            
 
             // Act - Perform an operation
             var result = Primes.FindPrimes(One);
 
             // Assert - Check for validity
-            Assert.IsTrue(empty.Count == result.Count);
+            Assert.IsTrue(empty.Count == result.Count && empty.Count == 0);
         }
     }
 }
